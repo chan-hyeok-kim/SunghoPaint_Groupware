33598b568e88f18e96b4f3b9414a66b20d1fc201
chan-hyeok-kim <132668682+chan-hyeok-kim@users.noreply.github.com>
2023-11-06 14:10:09 +0900

    Delete src/main/java/com/ham/len/humanResource directory

--- src/main/java/com/ham/len/humanResource/HumanResourceDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ham.len.humanResource;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface HumanResourceDAO {
-	public HumanResourceVO getHumanResource(String employeeID);
-	public int setRegistration(HumanResourceVO humanResourceVO);
-}
